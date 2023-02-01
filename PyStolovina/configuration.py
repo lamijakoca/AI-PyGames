a0a50dc57586b05369bf713c05bc6d64c0c5da00
lamijakoca <klamija99@gmail.com>
2023-02-01 12:51:01 +0100

    config params added

---
+++ PyStolovina/configuration.py
@@ -0,0 +1,20 @@
+import os
+
+GAME_FOLDER = os.path.dirname(__file__)
+IMAGES_FOLDER = os.path.join(GAME_FOLDER, 'images')
+MAPS_FOLDER = os.path.join(GAME_FOLDER, 'maps')
+
+MAX_HEIGHT = 600
+MAX_WIDTH = 1300    
+HEIGHT = None
+WIDTH = None
+SPEED = None
+
+TILE_SIZE = None
+FONT = None
+RIBBON_HEIGHT = None
+
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+GREEN = (90, 171, 97)
+RED = (236, 72, 66)
